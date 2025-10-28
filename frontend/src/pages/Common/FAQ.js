96e3519a9671caa2c20d30ef850a688b95ac1ffb
ojasbisht1962-lang <ojasbisht1962@gmail.com>
2025-10-28 10:15:51 +0530

    chore: remove unused PublicNavbar import from FAQ.js

--- frontend/src/pages/Common/FAQ.js
+++ frontend/src/pages/Common/FAQ.js
@@ -1,6 +1,5 @@
 import React, { useState } from 'react';
 import { useNavigate } from 'react-router-dom';
-import PublicNavbar from '../../components/PublicNavbar';
 import Footer from '../../components/Footer';
 import './FAQ.css';
 
